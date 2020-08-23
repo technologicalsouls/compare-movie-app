68d8adae13a59b5683d2f5e075596410c7fa012d
technologicalsouls <technologicalsouls@gmail.com>
2020-08-22 21:00:58 -0400

    search widget with results

--- index.js
+++ index.js
@@ -11,23 +11,53 @@ const fetchData = async (searchTerm) => {
         return [];
     };
 
-
     return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input"/>
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 //remember fetchData is async!
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
-    for (let movie of movies) {
-        const div = document.createElement('div');
 
-        div.innerHTML = `
-            <img src="${movie.Poster}"/>
-            <h1>${movie.Title}</h1>
+    if (!movies.length) {
+        dropdown.classList.remove('is-active');
+        return;
+    }
+    //check results-wrapper
+    resultsWrapper.innerHTML = '';
+    dropdown.classList.add('is-active');
+    for (let movie of movies) {
+        const option = document.createElement('a');
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+
+
+        option.classList.add('dropdown-item');
+        option.innerHTML = `
+            <img src="${imgSrc}"/>
+            ${movie.Title}
         `;
-        document.querySelector('#tar').appendChild(div);
+        resultsWrapper.appendChild(option);
     };
 };
 input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', event => {
+    // console.log(event.target);
+    if (!root.contains(event.target)) {
+        dropdown.classList.remove('is-active');
+    }
+});
