63e084a11f387175f6e159cc9432c7c1a6afc04e
technologicalsouls <technologicalsouls@gmail.com>
2020-08-29 15:57:07 -0400

    fix https

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const autoCompleteConfig = {
         return movie.Title;
     },
     async fetchData(searchTerm) {
-        const response = await axios.get('http://www.omdbapi.com/', {
+        const response = await axios.get('https://www.omdbapi.com/', {
             params: {
                 apikey: 'fa03a2fe',
                 s: searchTerm,
@@ -48,7 +48,7 @@ let leftMovie;
 let rightMovie;
 
 const onMovieSelect = async (movie, summaryElement, side) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
+    const response = await axios.get('https://www.omdbapi.com/', {
         params: {
             apikey: 'fa03a2fe',
             i: movie.imdbID
